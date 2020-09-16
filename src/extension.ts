246bf51822814e1f20d91c8c98ff3adbc0efe9fc
ljhao <ljhao177@outlook.com>
2020-09-16 16:21:15 +0800

    Modify base on wordcounter example,but not working

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,29 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	var WordCounter = require('./WordCounter');
+	var counter = new WordCounter(vscode);
+	//需要释放的资源都在这里依次push到这个数组里面
+	context.subscriptions.push(counter);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hello-world" is now active!');
+	// console.log('Congratulations, your extension "hello-world" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hello-world.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('hello-world.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hello World!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from Hello World!');
+	// });
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(counter);
 }
+exports.activate = activate;
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
+exports.deactivate = deactivate;
