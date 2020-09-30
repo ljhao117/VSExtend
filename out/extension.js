4615aa4150182a845b6f317fc521715ea196d63b
ljhao <ljhao177@outlook.com>
2020-09-30 11:46:18 +0800

    complete basic function of shortcut comment

--- out/extension.js
+++ out/extension.js
@@ -1,26 +1,20 @@
 "use strict";
-
-// import * as vscode from 'vscode';
-
 const vscode = require("vscode");
-// import {subscribeToDocumentChanges, EMOJI_MENTION} from './src/diagnostic';
 
 function activate(context) {
-    const disposable = vscode.commands.registerCommand('extension.reverseWord', function () {
+    const disposable = vscode.commands.registerCommand('extension.shortcutComment', function () {
         // Get the active text editor
         const editor = vscode.window.activeTextEditor;
 
+        const comment = '/***/';
         if (editor) {
-            const document = editor.document;
             const selection = editor.selection;
 
-            // Get the word within the selection
-            const word = document.getText(selection);
-            const reversed = word.split('').reverse().join('');
             editor.edit((editBuilder) => {
-                editBuilder.replace(selection, reversed);
+                editBuilder.replace(selection, comment);
             });
         }
+        
     });
     
     context.subscriptions.push(disposable);
